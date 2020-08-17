e43749cd4feda539200079f3bc79532fd1ea00d5
monuelDePari <roman.lynnyk.sa@gmail.com>
2020-08-17 16:12:35 +0300

    removed unused startupviewmodels code

--- JoinIT/JoinIT/Resourses/ViewModels/StartupViewModel.cs
+++ JoinIT/JoinIT/Resourses/ViewModels/StartupViewModel.cs
@@ -8,48 +8,6 @@
     using System.Windows.Input;
     public class StartupViewModel
     {
-        #region Constructors
-        public StartupViewModel()
-        {
-            //NewCoursesTabCommand = new RelativeCommand(p => NewCoursesTab());
-            //CoursesTabs = new ObservableCollection<CoursesTabViewModel>();
-
-            //CoursesTabs.CollectionChanged += CoursesTabsCollectionChanged;
-        }
-        #endregion
-
-        #region Commands
-        public ICommand NewCoursesTabCommand { get; private set; }
-        #endregion
-
-        #region Collections
-        public ObservableCollection<CoursesTabViewModel> CoursesTabs { get; private set; }
-        #endregion
-
-        #region Methods
-
-        private void CoursesTabsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            ICoursesTab coursesTab;
-
-            switch (e.Action)
-            {
-                case NotifyCollectionChangedAction.Add:
-                    coursesTab = (ICoursesTab)e.NewItems[0];
-                    coursesTab.CloseRequested += OnTabCloseRequested;
-                    break;
-                case NotifyCollectionChangedAction.Remove:
-                    coursesTab = (ICoursesTab)e.OldItems[0];
-                    coursesTab.CloseRequested -= OnTabCloseRequested;
-                    break;
-            }
-        }
-
-        private void OnTabCloseRequested(object sender, EventArgs e)
-        {
-            if (sender.GetType() == typeof(ICoursesTab))
-                CoursesTabs.Remove((CoursesTabViewModel)sender);
-        }
-        #endregion
+        
     }
 }
