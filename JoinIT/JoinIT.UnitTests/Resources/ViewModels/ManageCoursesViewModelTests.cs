f8a2450d1396fce1cb35f01dfc2afb3648f7dd86
monuelDePari <roman.lynnyk.sa@gmail.com>
2020-09-29 17:51:52 +0300

    test fix

--- JoinIT/JoinIT.UnitTests/Resources/ViewModels/ManageCoursesViewModelTests.cs
+++ JoinIT/JoinIT.UnitTests/Resources/ViewModels/ManageCoursesViewModelTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using JoinIT.Resources.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Models;
@@ -21,23 +20,15 @@ namespace JoinIT.UnitTests.Resources.ViewModels
         }
 
 
-        [DataTestMethod]
-        [DataRow("CPlusPlus", "Roman")]
-        public async Task SaveCourseAsync_IsAdding_AddEntityToDatabase(string courseName, string authorName)
+        [TestMethod]
+        public async Task SaveCourseAsync_IsAdding_AddEntityToDatabase()
         {
             //Arrange
-
-            var courseInfoModel = new CourseInfoModel();
 
             Mock<ICoursesRepository> repositoryMock = GetCoursesRepository();
             repositoryMock.Setup(t => t.AddAsync(It.IsAny<CourseInfoModel>()));
 
             ManageCoursesViewModel manageCoursesViewModel = GetViewModel(repositoryMock);
-            manageCoursesViewModel.CourseModel.CourseName = courseName;
-            manageCoursesViewModel.CourseModel.AuthorName = authorName;
-            manageCoursesViewModel.CourseModel.StartDate = new DateTime(2000, 1, 1);
-            manageCoursesViewModel.CourseModel.EndDate = new DateTime(2000, 1, 1);
-            
 
             //Act
 
@@ -45,7 +36,7 @@ namespace JoinIT.UnitTests.Resources.ViewModels
 
             //Assert
 
-            repositoryMock.Verify(t => t.AddAsync(courseInfoModel));
+            repositoryMock.Verify(t => t.AddAsync(It.IsAny<CourseInfoModel>()));
         }
     }
 }
